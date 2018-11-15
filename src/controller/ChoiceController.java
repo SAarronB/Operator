6a25bc3fff202588696e051d32f11e64a0ae7b80
SAaronB <programmingsean@gmail.com>
2018-11-15 08:01:10 -0700

    make list so you can call the methods and dreturn a massage

--- src/controller/ChoiceController.java
+++ src/controller/ChoiceController.java
@@ -1,8 +1,22 @@
 package controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChoiceController
 {
 	public void start() {
+		
+		
+		
+	}
+	
+	public  int lifeAmmoList() {
+		
+		List<Integer> lifeAmmo = new ArrayList<Integer>();
+		lifeAmmo.add(0, 100);
+		lifeAmmo.add(1, 32);
+		return 0;
 		
 	}
 	
